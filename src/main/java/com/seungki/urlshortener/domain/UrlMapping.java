fa3ce1a6bebb3f042e6ebec6e2a0125bde222963
seungki1011 <kimseungki1011@gmail.com>
2024-06-26 03:00:05 +0900

    Add entity for UrlMapping
    UrlMapping 엔티티 클래스 작성
    Track #7

---
+++ src/main/java/com/seungki/urlshortener/domain/UrlMapping.java
@@ -0,0 +1,36 @@
+package com.seungki.urlshortener.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class UrlMapping {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String hashValue;
+
+    @Column(nullable = false)
+    private String originalUrl;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime viewedAt;
+    private int viewCount;
+
+    /**
+     * 추후 회원 가입 기능을 추가할 때 사용
+     */
+    // @ManyToOne(fetch = FetchType.LAZY)
+    // @JoinColumn(name = "MEMBER_ID")
+    // private Member member;
+
+}
