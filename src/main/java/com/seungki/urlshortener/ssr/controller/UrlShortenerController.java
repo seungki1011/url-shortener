3a362d56913a034db29cb3fcdbac1bc1dfd511fc
seungki1011 <kimseungki1011@gmail.com>
2024-07-09 19:05:11 +0900

    Refactor ssr controller, view
    UrlShortenRequest를 사용하기 위해 리팩토링 진행
    issue #14

--- src/main/java/com/seungki/urlshortener/ssr/controller/UrlShortenerController.java
+++ src/main/java/com/seungki/urlshortener/ssr/controller/UrlShortenerController.java
@@ -4,10 +4,12 @@ import com.seungki.urlshortener.ssr.service.UrlShortenerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
@@ -17,13 +19,20 @@ public class UrlShortenerController {
     private final UrlShortenerService uss;
 
     @GetMapping({"/", "/shorten"})
-    public String shortenerForm() {
+    public String shortenerForm(Model model) {
+        model.addAttribute("urlShortenRequest", new UrlShortenRequest());
         return "shortener_form";
     }
 
     @PostMapping("/shorten")
-    public String shortenUrl(@RequestParam("url") String originalUrl, RedirectAttributes redirectAttributes) {
-        String shortcode = uss.shortenUrl(originalUrl);
+    public String shortenUrl(@ModelAttribute("urlShortenRequest") @Validated UrlShortenRequest usr,
+                             BindingResult bindingResult,
+                             RedirectAttributes redirectAttributes) {
+        if (bindingResult.hasErrors()) {
+            return "shortener_form";
+        }
+
+        String shortcode = uss.shortenUrl(usr.getUrl());
         redirectAttributes.addAttribute("shortcode", shortcode);
         return "redirect:/detail/{shortcode}";
     }
