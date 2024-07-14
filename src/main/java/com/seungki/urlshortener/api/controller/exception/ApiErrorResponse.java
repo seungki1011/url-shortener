a13e1d469345236dad03790430a29f97faf7723c
seungki1011 <kimseungki1011@gmail.com>
2024-07-15 08:25:43 +0900

    Refactor ErrorCode
    ErrorCode 구조 수정

--- src/main/java/com/seungki/urlshortener/api/controller/exception/ApiErrorResponse.java
+++ src/main/java/com/seungki/urlshortener/api/controller/exception/ApiErrorResponse.java
@@ -1,6 +1,8 @@
 package com.seungki.urlshortener.api.controller.exception;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 import lombok.Getter;
 
 @Getter
@@ -11,11 +13,18 @@ public class ApiErrorResponse {
     private String code;
     private String error;
     private String message;
+    private Map<String, Object> details;
 
-    public ApiErrorResponse(int status, ErrorCode errorCode, String message) {
-        this.status = status;
+    public ApiErrorResponse(ErrorCode errorCode, String message) {
+        this.status = errorCode.getStatus();
         this.code = errorCode.getCode();
         this.error = errorCode.getMessage();
         this.message = message;
+        this.details = new HashMap<>();
     }
+
+    public void addDetail(String key, Object value) {
+        this.details.put(key, value);
+    }
+
 }
--- src/main/java/com/seungki/urlshortener/api/controller/exception/ErrorCode.java
+++ src/main/java/com/seungki/urlshortener/api/controller/exception/ErrorCode.java
@@ -6,10 +6,12 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum ErrorCode {
-    SHORTCODE_NOT_FOUND("E001", "Shortcode Not Found"),
-    URL_NOT_FOUND("E002", "URL Not Found"),
-    SHORTCODE_GENERATION_FAILED("E003", "Shortcode Generation Failed");
+    VALIDATION_ERROR(400, "VALIDATION_ERROR", "입력값이 올바르지 않습니다. 검증에 실패하였습니다."),
+    SHORTCODE_NOT_FOUND(404, "SHORTCODE_NOT_FOUND", "해당 숏코드를 찾을 수 없습니다."),
+    URL_NOT_FOUND(404, "URL_NOT_FOUND", "해당 숏코드를 이용해 URL을 찾을 수 없습니다."),
+    SHORTCODE_GENERATION_FAILED(500, "SHORTCODE_GENERATION_FAILED", "중복된 숏코드에 대한 숏코드 재생성에 실패했습니다. 관리자에 문의를 해주세요.");
 
+    private final int status;
     private final String code;
     private final String message;
 }
